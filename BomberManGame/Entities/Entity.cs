c5831551cd891aed547a205134259e7eddc36a51
JoshuaWright42 <100580528@student.swin.edu.au>
2021-04-27 07:49:18 +1000

    Added Entities. Got missed last commit.

---
+++ BomberManGame/Entities/Entity.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace BomberManGame
+{
+    public abstract class Entity
+    {
+        private Dictionary<Type, Component> Components { get; init; }
+
+        public Entity(Dictionary<Type, Component> comps)
+        {
+            Components = comps;
+        }
+
+        public Component this[Type t] { get => Components[t]; }
+    }
+}
